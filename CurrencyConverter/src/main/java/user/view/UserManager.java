fb736fad49406e2b8a1401ef3d74d3e153230615
perttuj <perttuj@kth.se>
2017-12-08 17:26:09 +0100

    updated

--- CurrencyConverter/src/main/java/user/view/UserManager.java
+++ CurrencyConverter/src/main/java/user/view/UserManager.java
@@ -5,10 +5,76 @@
  */
 package user.view;
 
+import java.io.Serializable;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  *
  * @author Perttu Jääskeläinen
  */
-public class UserManager {
+@Named("userManager")
+@ConversationScoped
+public class UserManager implements Serializable {
+    @Inject
+    private Conversation conversation;
+    private String userID = "1";
+    private String toUpdate = null;
+    private boolean started = false;
     
+    public UserManager() {   
+    }
+    
+    public boolean getStarted() {
+        return this.started;
+    }
+    public boolean setStarted() {
+        return false;
+    }
+    
+    public String getUserID() {
+        return this.userID;
+    }
+    public void setUserID(String ID) {
+        this.userID = ID;
+    }
+    public void setUpdatedUser(String user) {
+        this.toUpdate = user;
+    }
+    public String getUpdatedUser() {
+        return null;
+    }
+    public void updateUser() {
+        if (this.toUpdate != null) {
+            return;
+        }
+        setUserID(this.toUpdate);
+        this.toUpdate = null;
+    }
+    public void removeUser() {
+        if (this.toUpdate == null) {
+            return;
+        }
+        setUserID(null);
+    }
+    /**
+     * Start a EJB session bean
+     */
+    public void startConversation() {
+        if (conversation.isTransient()) {
+            conversation.begin();
+            this.started = true;
+        }
+    }
+    /**
+     * End the EJB session bean
+     */
+    public void endConversation() {
+        if (!conversation.isTransient()) {
+            conversation.end();
+            this.started = false;
+        }
+    }
 }
